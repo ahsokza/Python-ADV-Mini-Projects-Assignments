9010fa3d65450420568a5f12f7af2241d4837651
khalid kanaan albreiki <khalid020908@gmail.com>
2026-02-11 23:40:50 +0400

    Restore deleted code in CAI611_M1_PC5.1-5.10.py
    Restored lost code in CAI611_M1_PC5.1-5.10.py due to accidental deletion.

---
+++ CAI611_M1_PC5.1-5.10.py
@@ -0,0 +1,20 @@
+#Full name: Khalid Kanaan Albreiki
+#Emirates ID number:
+#School ID: 302679
+#Task 1- Branch a
+
+#import libraries 
+import pandas as pd 
+import numpy as np 
+
+#load datasets into dataframes 
+flight_data = pd.read_csv('flight_data.csv')
+passenger_data = pd.read_csv('passenger_data.csv')
+booking_data = pd.read_csv('booking_data.csv')
+
+#display data
+flight_data.head()
+passenger_data.head()
+booking_data.head()
+
+#the rest of the code was lost because of this file being outdated, and i accidently deleted some of it. 
